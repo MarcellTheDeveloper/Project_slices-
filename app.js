b53c92fb758c1e3d620db4df30f68c5c1e953e84
Marcell Jeney <marcijeney@gmail.com>
2020-12-10 09:33:13 +0100

    project leírás

---
+++ app.js
@@ -0,0 +1,11 @@
+//Teljes project leírás : toDo with plus and minus, 10 ures mező alapböp de lehet tobbet,text input es torles
+
+//el kell tarolni h egy sornal mi lesz a html strukturaja
+
+//10x kell duplikalni az egy sort forral, hozzá  adni a 10x a root kezdő elementhez
+
+//gomb amivel hozzá tudunk adni még egyet, click eventlistenerrel, újra hozza adunk egy sort a listához
+
+//kijelölni class alapján az összes törlés gombot queryAll, és elmeni egy valtozoba
+
+//for cikl a törlés gombokra
